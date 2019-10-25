16b1dab68fa0bd1e742937545f8411e82d9e74af
Kristine Samsonyan <kisosamsonyan@yahoo.com>
2019-10-25 00:07:10 -0700

    draft 1

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include: ",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parantheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which of these IS NOT a javascript loop?",
+        choices: ["for", "circle", "do while", "while"],
+        answer: "circle"
+    },
+    {
+        title: "Which of these javascript operators means OR",
+        choices: ["||", "&&", "&", "%"],
+        answer: "||"
+    },
+{
+        title: "Which built-in method returns the length of the string?",
+        choices: ["lenght()", "size", "index()", "None of the above"],
+        answer: "length()"
+}
+
+    
+]
+
+
